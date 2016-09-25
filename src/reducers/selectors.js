a50384ecb7b9b94c9195e26639d9056840387332
David Van Gompel <david@unbrace.be>
2016-09-25 21:39:38 +0200

    [FEATURE] selectors - added currying allongside method chaining

--- src/reducers/selectors.js
+++ src/reducers/selectors.js
@@ -1,8 +1,41 @@
-export const getModel = state => state.repository;
+/* eslint arrow-parens: [0] */
+/* @flow */
+type ReduxState = Object;
+type Repository = Object;
+type PromiseStateData = Object | Array<Object>;
 
-export const getRepository = repositoryName => ({
-  fromState: state => state.repository[repositoryName] && state.repository[repositoryName].value,
-});
-export const getPromise = repositoryName => ({
-  fromState: state => state.repository[repositoryName],
-});
+type PromiseState = {
+  pending: boolean,
+  fulfilled: boolean,
+  rejected: boolean,
+  value: PromiseStateData,
+};
+
+export const getModel:Repository = (state:ReduxState) => state.repository;
+
+/**
+ * getRepository()
+ * usage:
+ *  - selectors.getRepository('repositoryName')(reduxState)
+ *  - selectors.getRepository('repositoryName').fromState(reduxState)
+ */
+export const getRepository:Function = (repositoryName:string) => {
+  const fromState:PromiseStateData = (state:ReduxState) =>
+    state.repository[repositoryName] && state.repository[repositoryName].value;
+  const ret:PromiseStateData = (state:ReduxState) => fromState(state);
+  ret.fromState = fromState;
+  return ret;
+};
+
+/**
+ * getPromise()
+ * usage:
+ *  - selectors.getPromise('repositoryName')(reduxState)
+ *  - selectors.getPromise('repositoryName').fromState(reduxState)
+ */
+export const getPromise:Function = (repositoryName:string) => {
+  const fromState:PromiseState = (state:ReduxState) => state.repository[repositoryName];
+  const ret:PromiseState = (state:ReduxState) => fromState(state);
+  ret.fromState = fromState;
+  return ret;
+};
