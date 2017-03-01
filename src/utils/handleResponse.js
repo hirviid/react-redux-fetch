6e99a2db9e435a2497445b010888ad2de2391be8
Dennis Meenhuis <github@dennus.net>
2017-03-01 09:08:27 +0100

    Reject the promise when the response is not OK

--- src/utils/handleResponse.js
+++ src/utils/handleResponse.js
@@ -1,18 +1,21 @@
 import newError from './errors';
 
 const handleResponse = (response) => {
+  if (!response.ok) {
+    return Promise.reject(newError(response.statusText ? response.statusText : 'Request failed'));
+  }
+
   if (response.headers.get('content-length') === '0' || response.status === 204) {
     return null;
   }
 
   const json = response.json();
 
-  return (response.status >= 200 && response.status < 300) ?
-    json
-    :
-    json.then(cause => (
-      Promise.reject(newError(cause))
-    ));
+  return response.ok
+    ? json
+    : json.then(cause => (
+        Promise.reject(newError(cause))
+      ));
 };
 
 export default handleResponse;
