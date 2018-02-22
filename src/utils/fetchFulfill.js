2c4357d6e58d617c5903c003c3516b58d452e69d
David Van Gompel <david@unbrace.be>
2018-02-22 11:51:42 +0100

    [fix] Convert Response to object before setting it to the state

--- src/utils/fetchFulfill.js
+++ src/utils/fetchFulfill.js
@@ -1,9 +1,11 @@
 import { FULFILLED } from '../constants/request';
+import responseToObject from './responseToObject';
 
-const fetchFulfill = (state, action) =>
-  state
+const fetchFulfill = (state, action) => state
     .merge(FULFILLED)
     .setIn(['value'], action.value)
-    .setIn(['meta'], action.request.meta);
+    .setIn(['meta'], action.request.meta)
+    .setIn(['meta', 'response'], responseToObject(action.request.meta.response));
+
 
 export default fetchFulfill;
